077a9ee5bb8efafb171334d8d6ad1189dd48e519
DeadKn1ght <deadkn1ght@bk.ru>
2023-04-10 16:19:24 +0300

    Added main file task1 from homework#6

---
+++ src/main/java/task1/task1.java
@@ -0,0 +1,30 @@
+package task1;
+
+import model.*;
+
+import java.util.*;
+
+import static model.Data.*;
+
+public class task1 {
+    public static void main(String[] args) {
+        List<Laptop> laptops = new ArrayList<Laptop>();
+        laptops.add(new MSI());
+        laptops.add(new Ilbrus());
+        laptops.add(new AppleMacBookPro());
+        laptops.add(new MSITitan());
+        Map<String, String> allSorted = choice();
+        System.out.println("Сортировка по:");
+        for (var entry:allSorted.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+        List<Laptop> sortList = suitableOptions(laptops, allSorted);
+        if(sortList.size() == 0){
+            System.out.println("Не подходящих вариантов");
+        }
+        for (Laptop laptop : sortList) {
+            System.out.println("Подходящие варианты: " + laptop.getClass().getSimpleName());
+        }
+    }
+
+}
