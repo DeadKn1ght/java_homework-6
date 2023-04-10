1c09de43326616e43a012e1040d19d7c2080607a
DeadKn1ght <deadkn1ght@bk.ru>
2023-04-10 16:22:06 +0300

    Added file MSITitan for homework#6 in folder model

---
+++ src/main/java/model/MSITitan.java
@@ -0,0 +1,33 @@
+package model;
+
+public class MSITitan implements Laptop{
+    @Override
+    public String producer() {
+        return "MSI";
+    }
+
+    @Override
+    public String color() {
+        return "black";
+    }
+
+    @Override
+    public Integer ram() {
+        return 32;
+    }
+
+    @Override
+    public Integer storage() {
+        return 2000;
+    }
+
+    @Override
+    public Boolean os() {
+        return true;
+    }
+
+    @Override
+    public Double price() {
+        return 47999.0;
+    }
+}
